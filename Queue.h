416a9ad7acfbf156fbc783ab5c11a0072a983091
whyfirefly <shmyaniv@mail.ru>
2021-02-16 17:51:49 +0300

    Добавление опций класса Queue (очередь)

--- Queue.h
+++ Queue.h
@@ -1,8 +1,80 @@
 #ifndef CONTAINER_QUEUE_H
 #define CONTAINER_QUEUE_H
 
+#include <vector>
+#include "Exception.h"
+#include "Stack.h"
 
-class Queue {
+using namespace std;
+
+template<class T>
+class Queue
+{
+private:
+    vector<T> container;
+
+public:
+    Queue() {}
+
+    Queue(vector<T> container) {
+        this->container = container;
+    }
+
+    //добавление элементов в очередь
+    void PushQ(T value)
+    {
+        container.push_back(value);
+    }
+
+    //удаление первого элемента из очереди (первый пришел->первый ушёл) - не сработало
+    T Remove() {
+        if (isEmpty()) {
+            throw Exception::Bottom;
+        } else {
+            //container.erase(container.begin() + index);
+            container.erase(&container[1]);
+        }
+    }
+
+    T FetchQ() {
+        while (!isEmpty()) {
+            // считываем данные , начиная с первого элемента очереди
+            auto line = move(container.front());
+        }
+    }
+
+    //пуста ли очередь?
+    bool isEmpty()
+    {
+        /*
+        if (container.empty()) {
+            return true;
+        } else {
+            return false;
+        }
+        */
+
+        return container.empty();
+    }
+
+    //чистка
+    void Clear()
+    {
+        container.clear();
+    }
+
+    void ForEach(void(*Func)(T))
+    {
+        for (auto i = container.rend()-1; i > container.rbegin()-1; i--) {
+            Func(*i);
+        }
+    }
+    void ForEach(T(*Func)(T))
+    {
+        for (auto i = container.rend()-1; i > container.rbegin()-1; i--) {
+            *i = Func(*i);
+        }
+    }
 
 };
 
