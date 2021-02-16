5cf82f337dc97e0455531970f7510bc8c89dd6bf
whyfirefly <shmyaniv@mail.ru>
2021-02-16 17:48:51 +0300

    Тестирование доработки класса Queue

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include "Stack.h"
 #include "utilites.h"
 #include "Exception.h"
+#include "Queue.h"
 
 using namespace std;
 
 int main() {
     system("chcp 65001");
 
-    Stack<int> stack;
+    /*Stack<int> stack;
     PrintEmpty(stack);
 
     stack.Push(5);
@@ -17,7 +18,27 @@ int main() {
 
     stack.ForEach(Print);
     stack.ForEach(Add10);
-    stack.ForEach(Print);
-    
+    stack.ForEach(Print);*/
+
+    Queue<int> queue;
+    PrintEmptyQ(queue);
+
+    queue.PushQ(6);
+    queue.PushQ(8);
+    queue.PushQ(9);
+    PrintEmptyQ(queue);
+
+    queue.ForEach(Print);
+    queue.ForEach(Add10);
+    queue.ForEach(Print);
+
+    //функция удаления не сработала :(
+    cout << endl;
+    queue.Remove();
+    queue.ForEach(Print);
+
+
+
+
     return 0;
 }
